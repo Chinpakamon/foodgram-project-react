d2c91cbba912ac29b4b6eb0ee740e196fc721296
Vladislav <vanya-005@mail.ru>
2023-05-13 16:00:35 +0300

    second_view

---
+++ backend/recipes/management/commands/load_data_json.py
@@ -0,0 +1,21 @@
+import json
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str)
+
+    def handle(self, *args, **options):
+        json_file = options['json_file']
+        Ingredient.objects.all().delete()
+
+        with open(json_file, encoding='utf-8') as x:
+            data = json.load(x)
+        for ingredient in data:
+            Ingredient.objects.create(
+                name=ingredient['name'],
+                unit=ingredient['measurement_unit']
+            )
