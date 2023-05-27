4a9635a61a3e2fb8bc68e8019962983e23769717
Vladislav <vanya-005@mail.ru>
2023-05-28 00:33:14 +0300

    second_view

--- backend/recipes/management/commands/load_data_json.py
+++ backend/recipes/management/commands/load_data_json.py
@@ -1,4 +1,4 @@
-import json
+import csv
 
 from django.core.management.base import BaseCommand
 
@@ -6,12 +6,28 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    help = 'Импорт данных из csv в модель Ingredient'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str, help='Путь к файлу')
+
     def handle(self, *args, **options):
-        with open('data/ingredients.json', 'rb') as f:
-            data = json.load(f)
-            for i in data:
-                ingredient = Ingredient()
-                ingredient.name = i['name']
-                ingredient.measurement_unit = i['measurement_unit']
-                ingredient.save()
-                print(i['name'], i['measurement_unit'])
+        print('Заполнение модели Ingredient из csv запущено.')
+        file_path = options['path'] + 'ingredients.csv'
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+
+            for row in reader:
+                try:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=row[0],
+                        measurement_unit=row[1],
+                    )
+                    if not created:
+                        print(
+                            f'Ингредиент {obj} уже существует в базе данных.'
+                        )
+                except Exception as error:
+                    print(f'Ошибка в строке {row}: {error}')
+
+        print('Заполнение модели Ingredient завершено.')
