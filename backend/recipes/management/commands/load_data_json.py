6824f39d7df5e90b263597f4de1b3d7cefd77ddc
Vladislav <vanya-005@mail.ru>
2023-05-28 00:36:19 +0300

    second_view

--- backend/recipes/management/commands/load_data_json.py
+++ backend/recipes/management/commands/load_data_json.py
@@ -6,28 +6,14 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Импорт данных из csv в модель Ingredient'
-
-    def add_arguments(self, parser):
-        parser.add_argument('--path', type=str, help='Путь к файлу')
+    help = '''Загрузка информации из csv-файла в базу данных.'''
 
     def handle(self, *args, **options):
-        print('Заполнение модели Ingredient из csv запущено.')
-        file_path = options['path'] + 'ingredients.csv'
-        with open(file_path, 'r') as csv_file:
-            reader = csv.reader(csv_file)
-
-            for row in reader:
-                try:
-                    obj, created = Ingredient.objects.get_or_create(
-                        name=row[0],
-                        measurement_unit=row[1],
-                    )
-                    if not created:
-                        print(
-                            f'Ингредиент {obj} уже существует в базе данных.'
-                        )
-                except Exception as error:
-                    print(f'Ошибка в строке {row}: {error}')
-
-        print('Заполнение модели Ingredient завершено.')
+        with open('ingredients.csv', encoding='utf-8') as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit
+                )
